5b02a460a05d95009a48fc17c45c1bd26645eb86
NicolasVeys <nicolas_veys@hotmail.com>
2020-06-11 15:09:05 +0200

    Index uitgavelijst

---
+++ Models/ExpeseCreateViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseWeb.Models
+{
+    public class ExpeseCreateViewModel
+    {
+        public int Id { get; set; }
+
+        [Range( 0,  9999999999999999999, ErrorMessage ="niet onder nul")]
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+
+    }
+}
